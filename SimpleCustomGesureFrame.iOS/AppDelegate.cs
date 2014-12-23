a4bb3b22c4c6d2cc18451dca06e2514f3f0734e9
Tomasz Kowalczyk <tomek@kownet.info>
2014-12-23 22:35:53 +0100

    ios project added

---
+++ SimpleCustomGesureFrame.iOS/AppDelegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+
+namespace SimpleCustomGesureFrame.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+		UIWindow window;
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			Forms.Init();
+
+			window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+			window.RootViewController = App.GetMainPage().CreateViewController();
+
+			window.MakeKeyAndVisible();
+
+			return true;
+		}
+	}
+}
---
+++ SimpleCustomGesureFrame.iOS/CustomRenderers/GestureFrameRenderer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using SimpleCustomGesureFrame;
+using SimpleCustomGesureFrame.iOS;
+using MonoTouch.UIKit;
+
+[assembly: ExportRenderer(typeof(GestureFrame), typeof(GestureFrameRenderer))]
+namespace SimpleCustomGesureFrame.iOS
+{
+	public class GestureFrameRenderer : FrameRenderer
+	{
+		UISwipeGestureRecognizer swipeDown;
+		UISwipeGestureRecognizer swipeUp;
+		UISwipeGestureRecognizer swipeLeft;
+		UISwipeGestureRecognizer swipeRight;
+
+		public GestureFrameRenderer()
+		{
+		}
+
+		protected override void OnElementChanged (ElementChangedEventArgs<Frame> e)
+		{
+			base.OnElementChanged (e);
+
+			swipeDown = new UISwipeGestureRecognizer (
+				() => 
+				{
+					GestureFrame _gi = (GestureFrame)this.Element;
+					_gi.OnSwipeDown();
+				}
+			)
+			{
+				Direction = UISwipeGestureRecognizerDirection.Down,
+			};
+
+			swipeUp = new UISwipeGestureRecognizer (
+				() => 
+				{
+					GestureFrame _gi = (GestureFrame)this.Element;
+					_gi.OnSwipeTop();
+				}
+			) 
+			{
+				Direction = UISwipeGestureRecognizerDirection.Up,
+			};
+
+			swipeLeft = new UISwipeGestureRecognizer (
+				() => 
+				{
+					GestureFrame _gi = (GestureFrame)this.Element;
+					_gi.OnSwipeLeft();
+				}
+			) 
+			{
+				Direction = UISwipeGestureRecognizerDirection.Left,
+			};
+
+			swipeRight = new UISwipeGestureRecognizer (
+				() => 
+				{
+					GestureFrame _gi = (GestureFrame)this.Element;
+					_gi.OnSwipeRight();
+				}
+			) 
+			{
+				Direction = UISwipeGestureRecognizerDirection.Right,
+			};
+
+			if (e.NewElement == null) {
+				if (swipeDown != null) {
+					this.RemoveGestureRecognizer (swipeDown);
+				}
+				if (swipeUp != null) {
+					this.RemoveGestureRecognizer (swipeUp);
+				}
+				if (swipeLeft != null) {
+					this.RemoveGestureRecognizer (swipeLeft);
+				}
+				if (swipeRight != null) {
+					this.RemoveGestureRecognizer (swipeRight);
+				}
+			}
+
+			if (e.OldElement == null) {
+				this.AddGestureRecognizer (swipeDown);
+				this.AddGestureRecognizer (swipeUp);
+				this.AddGestureRecognizer (swipeLeft);
+				this.AddGestureRecognizer (swipeRight);
+			}
+		}
+	}
+}
